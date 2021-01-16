d8555aaf156cd34c8e5ca5f33f369322c8ad3290
wahid sadique ayan <wsayan28@gmail.com>
2021-01-16 17:16:03 +0600

    add product table

---
+++ database/migrations/2021_01_16_105746_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('brand_id');
+            $table->unsignedInteger('unit_id');
+            $table->unsignedDecimal('price', 8, 2);
+            $table->string('name');
+            $table->string('description');
+            $table->string('sku');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
