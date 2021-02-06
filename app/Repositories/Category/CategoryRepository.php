8c436d58786960aac2b05831242150eefb4500a4
wahid sadique ayan <wsayan28@gmail.com>
2021-02-06 15:58:54 +0600

    update: category save image

--- app/Repositories/Category/CategoryRepository.php
+++ app/Repositories/Category/CategoryRepository.php
@@ -5,9 +5,12 @@ namespace App\Repositories\Category;
 
 
 use App\Category;
+use App\CategoryVImage;
+use App\Image;
 use App\Repositories\Category\ICategoryRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class CategoryRepository implements ICategoryRepository
@@ -29,6 +32,7 @@ class CategoryRepository implements ICategoryRepository
             'user_id' => 'required',
             'name' => 'required|string',
             'description' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
         if ($validator->fails()) {
@@ -40,19 +44,37 @@ class CategoryRepository implements ICategoryRepository
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        $category = new Category([
-            'name' => $request->get('name'),
-            'description' => $request->get('description'),
+        $postedImage = $request->file('image');
+        $imageName = uniqid('image-') . '-' . time() . '.' . $postedImage->getClientOriginalExtension();
+        $postedImage->storeAs('images', $imageName);
+        $image = new Image([
+            'image' => $imageName
         ]);
-
-        $category = $category->save();
-
-        if ($category == false) {
+        $status = $image->save();
+        if ($status == false) {
             return response()->json([
                 'success' => false,
                 'message' => 'Something went wrong!'
             ], 500);
         }
+
+        $category = new Category([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+        ]);
+        $status = $category->save();
+        if ($status == false) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong!'
+            ], 500);
+        }
+
+        $categoryVImage = new CategoryVImage([
+            'category_id' => $category->id,
+            'image_id' => $image->id,
+        ]);
+        $categoryVImage->save();
 
         return response()->json([
             'success' => true,
